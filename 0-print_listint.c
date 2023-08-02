1a07065e4e666c4523d22df8f3c5f262ed11c3f0
Adijat123 <adijbel@gmail.com>
2023-08-01 19:43:40 -0700

    0-print_listint.c

---
+++ 0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @l: linked list of type listint_t to print
+ *
+ * Return: number of nodes to print
+ */
+size_t print_listint(const listint_t *l)
+{
+	size_t new = 0;
+
+	while (l)
+	{
+		printf("%d\n", l->n);
+		num++;
+		l = l->next;
+	}
+
+	return (new);
+}
